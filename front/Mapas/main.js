00fe7dc94915f7da1877e5e82fe5f6fa71e11dc8
FelipeMatFa <Felipe2006mattosdefarias@gmail.com>
2024-07-30 18:07:43 -0300

    adicionar-local

--- front/Mapas/main.js
+++ front/Mapas/main.js
@@ -1,5 +1,11 @@
 const map = L.map('map').setView([-29.796508, -51.152714], 17);
 
+const button = document.querySelector("#botao-adicionar-local");
+button.onclick = () => {
+    const marker = L.marker([-29.796508, -51.152714]);
+    marker.addTo(map);
+    marker.bindPopup('Novo Local').openPopup();
+}
 
 
 const layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,6 +13,5 @@ const layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 });
 
 layer.addTo(map);
-
 // Link do projeto: https://leafletjs.com/
 // https://leaflet-extras.github.io/leaflet-providers/preview/
