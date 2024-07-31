587f6ec6c094b097d66d743c9be8f3f94f8c285f
FelipeMatFa <Felipe2006mattosdefarias@gmail.com>
2024-07-31 15:57:37 -0300

    mapa-com-leaflet.js

--- front/Mapas/main.js
+++ front/Mapas/main.js
@@ -1,17 +1,34 @@
-const map = L.map('map').setView([-29.796508, -51.152714], 17);
+const map = L.map('map').setView([51.505, -0.09], 13);
 
 const button = document.querySelector("#botao-adicionar-local");
 button.onclick = () => {
-    const marker = L.marker([-29.796508, -51.152714]);
+    const marker = L.marker([50, -50]);
     marker.addTo(map);
-    marker.bindPopup('Novo Local').openPopup();
+    marker.bindPopup('Restaurante Universitário').openPopup();
 }
-
 
 const layer = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19
 });
 
+function atualizarLocalizacao(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    L.marker([lat, lon]).addTo(map)
+        .bindPopup('Você está aqui!')
+        .openPopup();
+
+    map.setView([lat, lon], 13);
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(atualizarLocalizacao);
+} else {
+    alert('Geolocalização não é suportada por este navegador.');
+}
 layer.addTo(map);
+
 // Link do projeto: https://leafletjs.com/
 // https://leaflet-extras.github.io/leaflet-providers/preview/
+setTimeout(atualizarLocalizacao,1000)
